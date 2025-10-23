5b4579d95968c5f3a1e41a6fa72161f4e1372d1f
malailiyati <malailiyati107@gmail.com>
2025-10-24 06:18:48 +0700

    chore: setup React app entry with context providers and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { TodoProvider } from "./context/TodoContext";
+import { CategoryProvider } from "./context/CategoryContext";
+import Router from "./Router";
+import "antd/dist/reset.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <CategoryProvider>
+        <TodoProvider>
+          <Router />
+        </TodoProvider>
+      </CategoryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
